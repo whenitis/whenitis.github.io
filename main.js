8c4263f5c9dae1a77b11a2a0e83f224094158f85
whenitis <121204947+whenitis@users.noreply.github.com>
2022-12-22 01:25:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var deadline = new Date("Jan 1, 2023 0:0:0").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("time").innerHTML = days + "j " 
++ hours + "h " + minutes + "m " + seconds + "s ";
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("sentence").innerHTML = ""
+        document.getElementById("time").innerHTML = "On est en 2023 !";
+    }
+}, 1000);
